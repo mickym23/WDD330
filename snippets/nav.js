145eebfaab8feaf328c68726853ad6593e41d1f0
mickym23 <mikhail.cedras@gmail.com>
2021-09-25 17:53:56 +0200

    Fixed Broken Links

--- snippets/nav.js
+++ snippets/nav.js
@@ -1,2 +1,2 @@
-document.getElementById('navDiv').innerHTML = '<nav><ul><li><a href="/index.html">Home</a></li><li><a href="/views/notes.html">Weekly Notes</a></li><li><a href="/views/t-a.html">Team Activities</a></li><li><a href="/views/quiz/index.html">JS Ninja Quiz</a></li></ul></nav>' ;
+document.getElementById('navDiv').innerHTML = '<nav><ul><li><a href="/WDD330/index.html">Home</a></li><li><a href="/WDD330/views/notes.html">Weekly Notes</a></li><li><a href="/WDD330/views/t-a.html">Team Activities</a></li><li><a href="/WDD330/views/quiz/index.html">JS Ninja Quiz</a></li></ul></nav>' ;
 
