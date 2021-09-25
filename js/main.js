7ee07b8b86ab73e10b4a9f102e2f8017c06587e8
mickym23 <mikhail.cedras@gmail.com>
2021-09-25 18:45:45 +0200

    Trying PDF

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 const links = [
    {
       label: "Week 1 Notes",
-      url: "./index.html"
+      url: "/WDD330/pdf/week1notes.pdf",
+      pdf: "/WDD330/pdf/week1notes.pdf"
+   },
+   {
+      label: "Week 1 Notes",
+      url: "/WDD330/index.html",
+      pdf: "/WDD330/pdf/week1notes.pdf"
    }
 ];
 
