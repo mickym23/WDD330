363e46b96b75846933d47b3ad70a78a03c8e3736
mickym23 <mikhail.cedras@gmail.com>
2021-09-25 17:01:37 +0200

    Added Site Updates

---
+++ snippets/nav.js
@@ -0,0 +1,2 @@
+document.getElementById('navDiv').innerHTML = '<nav><ul><li><a href="/index.html">Home</a></li><li><a href="/views/notes.html">Weekly Notes</a></li><li><a href="/views/t-a.html">Team Activities</a></li><li><a href="/views/quiz/index.html">JS Ninja Quiz</a></li></ul></nav>' ;
+
