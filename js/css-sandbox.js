8ed80b42991660959ea8f569999421a309f40129
mickym23 <mikhail.cedras@gmail.com>
2021-11-06 15:04:23 +0200

    Added Week 8 code

---
+++ js/css-sandbox.js
@@ -0,0 +1,23 @@
+function draw() {
+   var canvas = document.getElementById('canvas');
+   if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+     
+ 
+     ctx.beginPath();
+     ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+     ctx.fillStyle = "#FFFF00";
+     ctx.fill();
+     ctx.moveTo(110, 75);
+     //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+     ctx.arc(75, 75, 35, 0*Math.PI, 0.50*Math.PI, false);  // Mouth (clockwise)
+     ctx.moveTo(65, 65);
+     ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+     ctx.moveTo(95, 65);
+     ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+     
+     ctx.stroke();
+   }
+ }
+ 
+ window.addEventListener('DOMContentLoaded', draw);
