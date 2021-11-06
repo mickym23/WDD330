2a4fa5285f33107924f874a53556409d34a13f77
mickym23 <mikhail.cedras@gmail.com>
2021-11-06 15:07:21 +0200

    Added Week 8 code

--- js/css-sandbox.js
+++ js/css-sandbox.js
@@ -1,16 +1,19 @@
+/* Went through tutorial with:
+   https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
+   https://www.w3schools.com/tags/canvas_arc.asp
+*/
 function draw() {
-   var canvas = document.getElementById('canvas');
+   let canvas = document.getElementById('canvas');
    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-     
- 
+     let ctx = canvas.getContext('2d');
+   
      ctx.beginPath();
      ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-     ctx.fillStyle = "#FFFF00";
-     ctx.fill();
+     ctx.fillStyle = "#FFFF00"; 
+     ctx.fill(); // Fill circle with yellow
      ctx.moveTo(110, 75);
-     //context.arc(x,y,r,sAngle,eAngle,counterclockwise);
-     ctx.arc(75, 75, 35, 0*Math.PI, 0.50*Math.PI, false);  // Mouth (clockwise)
+     //Arc params: context.arc(x,y,radius,startingAngle,endingAngle,counterclockwise);
+     ctx.arc(75, 75, 35, 0*Math.PI, 0.50*Math.PI, false);  // Mouth
      ctx.moveTo(65, 65);
      ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
      ctx.moveTo(95, 65);
